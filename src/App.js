91b52f17613514ec537f1fce719bd00352ddc0f3
Rajan Rahman <36062541+rajanrahman@users.noreply.github.com>
2019-11-25 21:36:09 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>@pastorlik</h3>
+      <a href="https://itunes.apple.com/us/app/simple-radio-by-streema-tunein/id891132290?mt=8">
+        Rehema Radio via Simple Radio App
+      </a>
+      <a href="https://mobile.twitter.com/pastor_lik">Twitter</a>
     </div>
   );
 }
